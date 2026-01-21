e15ce1f48ba979ab63399e8a03174514b68ec9cc
viquezcodedav <student-03-8ce725bd23d2@qwiklabs.net>
2026-01-21 22:10:56 +0000

    Testing Build Trigger

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 @app.route("/")
 def main():
-    model = {"title": "Hello DevOps Fans."}
+    model = {"title": "Hello Build Trigger."}
     return render_template('index.html', model=model)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
